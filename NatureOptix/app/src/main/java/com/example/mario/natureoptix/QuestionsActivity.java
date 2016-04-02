26a3f83891bc34399cb4d1783e95b857169d9d1c
calcem <calcem2@mcmaster.ca>
2016-04-02 15:43:20 -0400

    Created basic UIs for the main menu, questions activity and displaying results

--- NatureOptix/app/src/main/java/com/example/mario/natureoptix/QuestionsActivity.java
+++ NatureOptix/app/src/main/java/com/example/mario/natureoptix/QuestionsActivity.java
@@ -1,7 +1,9 @@
 package com.example.mario.natureoptix;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class QuestionsActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class QuestionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+    }
+
+    public void getResults(View v){
+        Intent resultsIntent =new Intent(this,ResultsActivity.class);
+        startActivity(resultsIntent);
     }
 }
